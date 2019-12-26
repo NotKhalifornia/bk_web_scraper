46b68ee53cb4fafd90d1b305dcc775b2ca06b9d7
rg <ryan@gedwill.com>
2019-12-26 11:10:11 -0800

    changed location to scrape

--- web_scraper/spiders/yp_dentists_spider.py
+++ web_scraper/spiders/yp_dentists_spider.py
@@ -13,7 +13,7 @@ class YpDentistsSpiderSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        url = 'https://www.yellowpages.com/search?search_terms=salon&geo_location_terms=Los%20Angeles%2C%20CA&page=' + str(self.page_id)
+        url = 'https://www.yellowpages.com/search?search_terms=restaurant&geo_location_terms=San%20Jose%2C%20CA&page=' + str(self.page_id)
         yield scrapy.Request(url=url, callback=self.parse_results)
 
     def parse_results(self, response):
